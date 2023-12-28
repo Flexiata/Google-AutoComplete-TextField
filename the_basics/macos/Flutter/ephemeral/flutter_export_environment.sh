9e592b444f742c21c521fd333bc4c236b6b83407
supunnilakshana <supunnilakshana1999@gmail.com>
2023-12-28 14:37:26 +0530

    WIP

---
+++ the_basics/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\supun\fvm\versions\3.10.6"
+export "FLUTTER_APPLICATION_PATH=C:\Supun\Professional\Development\Felxiata\custom_pkg_repos\Google-AutoComplete-TextField-Flutter\the_basics"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
